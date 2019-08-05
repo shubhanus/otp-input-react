a2865157f74380976cff22e8c4b6f274d8e24260
shubhanu <shubhanu@cartoonmango.com>
2019-08-05 23:37:17 +0530

    Updates

---
+++ precache-manifest.7ecbbbe812a665673e004f7450ee039b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c69f14dee024939ee4fd8aaf3f7dad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd3309e02f8bfd2f1a19",
+    "url": "/static/css/main.7578fedb.chunk.css"
+  },
+  {
+    "revision": "fee0ff90189f30bad446",
+    "url": "/static/js/2.1feaa788.chunk.js"
+  },
+  {
+    "revision": "cd3309e02f8bfd2f1a19",
+    "url": "/static/js/main.a21c4599.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
