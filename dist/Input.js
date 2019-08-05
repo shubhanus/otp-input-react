7565225837c40e0b1e7c943126bf623a81bdd0f2
shubhanu <shubhanu@cartoonmango.com>
2019-08-05 19:01:49 +0530

    updated version and babel issue

--- dist/Input.js
+++ dist/Input.js
@@ -1,26 +1,43 @@
-function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+"use strict";
 
-import React, { useEffect, useRef } from "react";
-import PropTypes from "prop-types";
-import { isStyleObject } from ".";
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-const Input = ({
-  focus,
-  shouldAutoFocus,
-  separator,
-  isLastChild,
-  inputStyle,
-  isDisabled,
-  hasErrored,
-  errorStyle,
-  focusStyle,
-  disabledStyle,
-  isInputNum,
-  value,
-  ...rest
-}) => {
-  const input = useRef(null);
-  useEffect(() => {
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require("prop-types");
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _ = require(".");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+var Input = function Input(_ref) {
+  var focus = _ref.focus,
+      shouldAutoFocus = _ref.shouldAutoFocus,
+      separator = _ref.separator,
+      isLastChild = _ref.isLastChild,
+      inputStyle = _ref.inputStyle,
+      isDisabled = _ref.isDisabled,
+      hasErrored = _ref.hasErrored,
+      errorStyle = _ref.errorStyle,
+      focusStyle = _ref.focusStyle,
+      disabledStyle = _ref.disabledStyle,
+      isInputNum = _ref.isInputNum,
+      value = _ref.value,
+      rest = _objectWithoutProperties(_ref, ["focus", "shouldAutoFocus", "separator", "isLastChild", "inputStyle", "isDisabled", "hasErrored", "errorStyle", "focusStyle", "disabledStyle", "isInputNum", "value"]);
+
+  var input = (0, _react.useRef)(null);
+
+  (0, _react.useEffect)(function () {
     // Focus on first render
     // Only when shouldAutoFocus is true
     // Prevent calling function if input already in focus
@@ -29,47 +46,51 @@ const Input = ({
     }
   }, [focus]);
 
-  const getClasses = (...classes) => classes.filter(c => !isStyleObject(c) && c !== false).join(" ");
-
-  const numValueLimits = isInputNum ? {
-    min: 0,
-    max: 9
-  } : {};
-  return React.createElement("div", {
-    style: {
-      display: "flex",
-      alignItems: "center"
+  var getClasses = function getClasses() {
+    for (var _len = arguments.length, classes = Array(_len), _key = 0; _key < _len; _key++) {
+      classes[_key] = arguments[_key];
     }
-  }, React.createElement("input", _extends({
-    style: Object.assign({
-      width: "1em",
-      textAlign: "center"
-    }, isStyleObject(inputStyle) && inputStyle, focus && isStyleObject(focusStyle) && focusStyle, isDisabled && isStyleObject(disabledStyle) && disabledStyle, hasErrored && isStyleObject(errorStyle) && errorStyle),
-    className: getClasses(inputStyle, focus && focusStyle, isDisabled && disabledStyle, hasErrored && errorStyle),
-    type: isInputNum ? "number" : "tel"
-  }, numValueLimits, {
-    maxLength: "1",
-    ref: input,
-    disabled: isDisabled,
-    value: value || ""
-  }, rest)), !isLastChild && separator);
+
+    return classes.filter(function (c) {
+      return !(0, _.isStyleObject)(c) && c !== false;
+    }).join(" ");
+  };
+
+  var numValueLimits = isInputNum ? { min: 0, max: 9 } : {};
+
+  return _react2.default.createElement(
+    "div",
+    { style: { display: "flex", alignItems: "center" } },
+    _react2.default.createElement("input", _extends({
+      style: Object.assign({ width: "1em", textAlign: "center" }, (0, _.isStyleObject)(inputStyle) && inputStyle, focus && (0, _.isStyleObject)(focusStyle) && focusStyle, isDisabled && (0, _.isStyleObject)(disabledStyle) && disabledStyle, hasErrored && (0, _.isStyleObject)(errorStyle) && errorStyle),
+      className: getClasses(inputStyle, focus && focusStyle, isDisabled && disabledStyle, hasErrored && errorStyle),
+      type: isInputNum ? "number" : "tel"
+    }, numValueLimits, {
+      maxLength: "1",
+      ref: input,
+      disabled: isDisabled,
+      value: value || ""
+    }, rest)),
+    !isLastChild && separator
+  );
 };
 
 Input.propTypes = {
-  focus: PropTypes.bool,
-  shouldAutoFocus: PropTypes.bool,
-  numInputs: PropTypes.number,
-  onChange: PropTypes.func,
-  separator: PropTypes.object,
-  containerStyle: PropTypes.object,
-  inputStyle: PropTypes.object,
-  focusStyle: PropTypes.object,
-  isDisabled: PropTypes.bool,
-  disabledStyle: PropTypes.object,
-  hasErrored: PropTypes.bool,
-  errorStyle: PropTypes.object,
-  isInputNum: PropTypes.bool,
-  value: PropTypes.string,
-  isLastChild: PropTypes.bool
+  focus: _propTypes2.default.bool,
+  shouldAutoFocus: _propTypes2.default.bool,
+  numInputs: _propTypes2.default.number,
+  onChange: _propTypes2.default.func,
+  separator: _propTypes2.default.object,
+  containerStyle: _propTypes2.default.object,
+  inputStyle: _propTypes2.default.object,
+  focusStyle: _propTypes2.default.object,
+  isDisabled: _propTypes2.default.bool,
+  disabledStyle: _propTypes2.default.object,
+  hasErrored: _propTypes2.default.bool,
+  errorStyle: _propTypes2.default.object,
+  isInputNum: _propTypes2.default.bool,
+  value: _propTypes2.default.string,
+  isLastChild: _propTypes2.default.bool
 };
-export default React.memo(Input);
+
+exports.default = _react2.default.memo(Input);
--- dist/index.js
+++ dist/index.js
@@ -1,75 +1,109 @@
-// @flow
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import Input from "./Input"; // keyCode constants
+"use strict";
 
-const BACKSPACE = 8;
-const LEFT_ARROW = 37;
-const RIGHT_ARROW = 39;
-const DELETE = 46; // Doesn't really check if it's a style Object
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.isStyleObject = undefined;
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require("prop-types");
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _Input = require("./Input");
+
+var _Input2 = _interopRequireDefault(_Input);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// keyCode constants
+var BACKSPACE = 8;
+var LEFT_ARROW = 37;
+var RIGHT_ARROW = 39;
+var DELETE = 46;
+
+// Doesn't really check if it's a style Object
 // Basic implemenetation to check if it's not a string
 // of classNames and is an Object
 // TODO: Better implementation
+var isStyleObject = exports.isStyleObject = function isStyleObject(obj) {
+  return (typeof obj === "undefined" ? "undefined" : _typeof(obj)) === "object";
+};
 
-export const isStyleObject = obj => typeof obj === "object";
+var OtpInput = function OtpInput(_ref) {
+  var numInputs = _ref.numInputs,
+      inputStyle = _ref.inputStyle,
+      focusStyle = _ref.focusStyle,
+      separator = _ref.separator,
+      isDisabled = _ref.isDisabled,
+      disabledStyle = _ref.disabledStyle,
+      hasErrored = _ref.hasErrored,
+      errorStyle = _ref.errorStyle,
+      shouldAutoFocus = _ref.shouldAutoFocus,
+      value = _ref.value,
+      onChange = _ref.onChange,
+      isInputNum = _ref.isInputNum,
+      containerStyle = _ref.containerStyle;
 
-const OtpInput = ({
-  numInputs,
-  inputStyle,
-  focusStyle,
-  separator,
-  isDisabled,
-  disabledStyle,
-  hasErrored,
-  errorStyle,
-  shouldAutoFocus,
-  value,
-  onChange,
-  isInputNum,
-  containerStyle
-}) => {
-  const [activeInput, setActiveInput] = useState(0);
+  var _useState = (0, _react.useState)(0),
+      _useState2 = _slicedToArray(_useState, 2),
+      activeInput = _useState2[0],
+      setActiveInput = _useState2[1];
 
-  const getOtpValue = () => value ? value.toString().split("") : []; // Helper to return OTP from input
+  var getOtpValue = function getOtpValue() {
+    return value ? value.toString().split("") : [];
+  };
 
-
-  const handleOtpChange = otp => {
-    const otpValue = otp.join("");
+  // Helper to return OTP from input
+  var handleOtpChange = function handleOtpChange(otp) {
+    var otpValue = otp.join("");
     onChange(isInputNum ? Number(otpValue) : otpValue);
-  }; // Focus on input by index
+  };
 
-
-  const focusInput = input => {
-    const nextActiveInput = Math.max(Math.min(numInputs - 1, input), 0);
+  // Focus on input by index
+  var focusInput = function focusInput(input) {
+    var nextActiveInput = Math.max(Math.min(numInputs - 1, input), 0);
     setActiveInput(nextActiveInput);
-  }; // Focus on next input
+  };
 
-
-  const focusNextInput = () => {
+  // Focus on next input
+  var focusNextInput = function focusNextInput() {
     focusInput(activeInput + 1);
-  }; // Focus on previous input
+  };
 
-
-  const focusPrevInput = () => {
+  // Focus on previous input
+  var focusPrevInput = function focusPrevInput() {
     focusInput(activeInput - 1);
-  }; // Change OTP value at focused input
+  };
 
+  // Change OTP value at focused input
+  var changeCodeAtFocus = function changeCodeAtFocus(_ref2) {
+    var _ref3 = _slicedToArray(_ref2, 1),
+        nextValue = _ref3[0];
 
-  const changeCodeAtFocus = ([nextValue]) => {
-    const otp = getOtpValue();
+    var otp = getOtpValue();
     otp[activeInput] = nextValue;
     handleOtpChange(otp);
-  }; // Handle pasted OTP
+  };
 
-
-  const handleOnPaste = e => {
+  // Handle pasted OTP
+  var handleOnPaste = function handleOnPaste(e) {
     e.preventDefault();
-    const otp = getOtpValue(); // Get pastedData in an array of max size (num of inputs - current position)
+    var otp = getOtpValue();
 
-    const pastedData = e.clipboardData.getData("text/plain").slice(0, numInputs - activeInput).split(""); // Paste data from focused input onwards
+    // Get pastedData in an array of max size (num of inputs - current position)
+    var pastedData = e.clipboardData.getData("text/plain").slice(0, numInputs - activeInput).split("");
+
+    // Paste data from focused input onwards
     // eslint-disable-next-line no-plusplus
-
-    for (let pos = 0; pos < numInputs; ++pos) {
+    for (var pos = 0; pos < numInputs; ++pos) {
       if (pos >= activeInput && pastedData.length > 0) {
         otp[pos] = pastedData.shift();
       }
@@ -78,18 +112,17 @@ const OtpInput = ({
     handleOtpChange(otp);
   };
 
-  const handleOnChange = e => {
+  var handleOnChange = function handleOnChange(e) {
     if (isInputNum && Number.isNaN(Number(e.target.value))) {
       // preventing number other then number inputs
       return;
     }
-
     changeCodeAtFocus(e.target.value);
     focusNextInput();
-  }; // Handle cases of backspace, delete, left arrow, right arrow
+  };
 
-
-  const handleOnKeyDown = e => {
+  // Handle cases of backspace, delete, left arrow, right arrow
+  var handleOnKeyDown = function handleOnKeyDown(e) {
     if (e.keyCode === BACKSPACE || e.key === "Backspace") {
       e.preventDefault();
       changeCodeAtFocus("");
@@ -106,19 +139,21 @@ const OtpInput = ({
     }
   };
 
-  const checkLength = e => {
+  var checkLength = function checkLength(e) {
     if (e.target.value.length > 1) {
       e.preventDefault();
       focusNextInput();
     }
   };
 
-  const renderInputs = () => {
-    const otp = getOtpValue();
-    const inputs = []; // eslint-disable-next-line no-plusplus
+  var renderInputs = function renderInputs() {
+    var otp = getOtpValue();
+    var inputs = [];
 
-    for (let i = 0; i < numInputs; i++) {
-      inputs.push(React.createElement(Input, {
+    // eslint-disable-next-line no-plusplus
+
+    var _loop = function _loop(i) {
+      inputs.push(_react2.default.createElement(_Input2.default, {
         key: i,
         focus: activeInput === i,
         value: otp && otp[i],
@@ -126,11 +161,13 @@ const OtpInput = ({
         onKeyDown: handleOnKeyDown,
         onInput: checkLength,
         onPaste: handleOnPaste,
-        onFocus: e => {
+        onFocus: function onFocus(e) {
           setActiveInput(i);
           e.target.select();
         },
-        onBlur: () => setActiveInput(-1),
+        onBlur: function onBlur() {
+          return setActiveInput(-1);
+        },
         separator: separator,
         inputStyle: inputStyle,
         focusStyle: focusStyle,
@@ -142,39 +179,47 @@ const OtpInput = ({
         shouldAutoFocus: shouldAutoFocus,
         isInputNum: false
       }));
+    };
+
+    for (var i = 0; i < numInputs; i++) {
+      _loop(i);
     }
 
     return inputs;
   };
 
-  return React.createElement("div", {
-    style: Object.assign({
-      display: "flex"
-    }, isStyleObject(containerStyle) && containerStyle),
-    className: !isStyleObject(containerStyle) && containerStyle
-  }, renderInputs());
+  return _react2.default.createElement(
+    "div",
+    {
+      style: Object.assign({ display: "flex" }, isStyleObject(containerStyle) && containerStyle),
+      className: !isStyleObject(containerStyle) && containerStyle
+    },
+    renderInputs()
+  );
 };
 
 OtpInput.propTypes = {
-  numInputs: PropTypes.number,
-  onChange: PropTypes.func,
-  separator: PropTypes.object,
-  containerStyle: PropTypes.object,
-  inputStyle: PropTypes.object,
-  focusStyle: PropTypes.object,
-  isDisabled: PropTypes.bool,
-  disabledStyle: PropTypes.object,
-  hasErrored: PropTypes.bool,
-  errorStyle: PropTypes.object,
-  shouldAutoFocus: PropTypes.bool,
-  isInputNum: PropTypes.bool,
-  value: PropTypes.string
+  numInputs: _propTypes2.default.number,
+  onChange: _propTypes2.default.func,
+  separator: _propTypes2.default.object,
+  containerStyle: _propTypes2.default.object,
+  inputStyle: _propTypes2.default.object,
+  focusStyle: _propTypes2.default.object,
+  isDisabled: _propTypes2.default.bool,
+  disabledStyle: _propTypes2.default.object,
+  hasErrored: _propTypes2.default.bool,
+  errorStyle: _propTypes2.default.object,
+  shouldAutoFocus: _propTypes2.default.bool,
+  isInputNum: _propTypes2.default.bool,
+  value: _propTypes2.default.string
 };
+
 OtpInput.defaultProps = {
   numInputs: 4,
-  onChange: () => {},
+  onChange: function onChange() {},
   isDisabled: false,
   shouldAutoFocus: false,
   value: ""
 };
-export default OtpInput;
+
+exports.default = OtpInput;
