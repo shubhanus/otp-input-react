96c82a37a20dffa0692168d3cfb5e2018cdb7b01
Shubhanu Sharma <shubhanu@locus.sh>
2019-09-16 11:38:57 +0530

    Updates

---
+++ precache-manifest.2eaae87fe504e4a486476150420364c6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458146dc4a25f2a4a1025a4d08f73a78",
+    "url": "/otp-input-react/index.html"
+  },
+  {
+    "revision": "ab8f6850d1a39452600b",
+    "url": "/otp-input-react/static/js/2.4648f552.chunk.js"
+  },
+  {
+    "revision": "5024124ccb0fcacee8e4",
+    "url": "/otp-input-react/static/js/main.40474020.chunk.js"
+  },
+  {
+    "revision": "bc9472fbfb0ac030eb64",
+    "url": "/otp-input-react/static/js/runtime~main.83e8a476.js"
+  }
+]);
